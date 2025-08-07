d296bdd96b62868aa363fc6205d5e0b803611df6
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-08-08 01:44:45 +0200

    secret

--- Backend/utils/auth.js
+++ Backend/utils/auth.js
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { pool } from "./config.js";
 
 export const auth = betterAuth({
+  secret: process.env.SECRET,
   database: pool,
   emailAndPassword: {
     enabled: true,
@@ -11,6 +12,12 @@ export const auth = betterAuth({
       prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      secure: true,
+      sameSite: "none",
     },
   },
   trustedOrigins: ["http://localhost:5173"],
