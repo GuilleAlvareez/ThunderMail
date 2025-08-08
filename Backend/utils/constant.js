b4dec4c1f5484efe70146542f1ddf6e36bf756ec
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-08-08 02:00:50 +0200

    origen de cors actualizado

--- Backend/utils/constant.js
+++ Backend/utils/constant.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 export const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://thunder-mail.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With']
