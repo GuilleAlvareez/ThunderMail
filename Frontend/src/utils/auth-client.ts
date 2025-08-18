aa7a4798d94de006a8834480df6e76a016493975
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-08-18 21:01:26 +0200

    ubicacion backend

--- Frontend/src/utils/auth-client.ts
+++ Frontend/src/utils/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
-  // Le decimos al cliente que nuestro backend está en localhost:3000
-  baseURL: import.meta.env.VITE_URL_BACKEND,
-  // baseURL: "http://localhost:3000",
+  // Le decimos al cliente que nuestro backend está en localhost:6543
+  // baseURL: import.meta.env.VITE_URL_BACKEND,
+  baseURL: "http://localhost:6543",
 });
